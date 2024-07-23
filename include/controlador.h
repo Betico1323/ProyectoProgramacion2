58de43041de546c1c8564b35a251367df7ec535b
jdvillasmil <jdvillasmil007@gmail.com>
2024-07-22 23:45:26 -0400

    feat: Añadiendo logica para controlador.h, vista.h y error.log

---
+++ include/controlador.h
@@ -0,0 +1,9 @@
+#ifndef CONTROLADOR_H
+#define CONTROLADOR_H
+
+#include "usuario.h"
+#include "libro.h"
+
+void manejarErrores(const string& mensaje);
+
+#endif
---
+++ include/vista.h
@@ -0,0 +1,10 @@
+#ifndef VISTA_H
+#define VISTA_H
+
+#include "usuario.h"
+#include "libro.h"
+
+void mostrarMenu(const Usuario* usuarioActual);
+void mostrarMensaje(const string& mensaje);
+
+#endif
